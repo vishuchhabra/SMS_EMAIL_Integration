63c46e1ba5245817041e17b715fcbdd232e8651f
vishuchhabra <vishuchhabra1016@gmail.com>
2020-06-28 16:03:35 +0530

    Both are working fine

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const Nexmo = require('nexmo');
+//for environment variable
+require('dotenv').config()
+const sgMail = require('@sendgrid/mail');
+
+//for messaging 
 
 const nexmo = new Nexmo({
-  apiKey: 'd325995d',
-  apiSecret: 'yHKAabAbGkC9eUSp',
+  apiKey: '',
+  apiSecret:'',
 });
 
 const from = 'Vonage APIs';
 const to = '916283032071';
-const text = "https://www.w3schools.com"
+const text = "https://google.com"
 
 nexmo.message.sendSms(from, to, text, {
     type: "unicode"
@@ -19,3 +24,34 @@ nexmo.message.sendSms(from, to, text, {
           console.log(`Message has successfully sent to ${to} `)
       }
   })
+
+
+//for mailing purposes
+
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+const msg = {
+  to: 'vishuintern@gmail.com',
+  from: 'vishuchhabra1016@gmail.com',
+  subject: 'This is Just for Testing',
+  text: 'Hii , This is vishu chhabra ',
+  html: `<h1>This is vishu chhabra <h1> <a href="https://google.com"> <button style="background-color: #4CAF50; /* Green */
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;" >CLICK HERE</button> </a>`,
+};
+sgMail.send(msg).then(res =>{
+ 
+    console.log("Email sent successfully")
+
+}).catch(err =>{
+    console.log("There is an error")
+})
+
+
+ 
+
